30fff6d36627253f5b31293f127375a6eb599032
JMATHEWS <JMATHEWS@JMATHEWS-IN.in.oracle.com>
2017-08-31 13:41:14 +0530

    login update

--- src/main/webapp/scripts/controllers/login.js
+++ src/main/webapp/scripts/controllers/login.js
@@ -5,8 +5,33 @@ app.controller('loginCtrl', function($scope,$http) {
 			//alert(response.statusText);
 			//alert(response.status);
 			//alert(response.data.users);
-			$scope.userList = response.data.users;     
-			window.location = 'views/partials/home/home.html';
+			$scope.userList = response.data.users;
+			//alert($scope.userList[0].name);
+			//alert($scope.password);
+			var validUser = false;
+			$scope.message="incorrect credentials";
+			for(var count = 0; count < $scope.userList.length; count++){
+				//alert("response name: "+$scope.userList[count].name);
+				//alert("response password: "+$scope.userList[count].password);
+				//alert("entered name: "+$scope.userid);
+				//alert("entered password: "+$scope.password);
+				//alert($scope.password==$scope.userList[count].password);
+				if($scope.password==$scope.userList[count].password && $scope.userid==$scope.userList[count].name){
+					//alert("validated");
+					validUser = true;
+					break;					
+				}		
+
+			}
+			//alert(validUser);
+			if(validUser==true){
+				//alert("in true");
+				window.location = 'views/partials/home/home.html';
+			}else{
+				//alert("in false");
+				window.location = 'index.html';
+			}
+			//
 		}, function(response) {
 			//Second function handles error
 			$scope.message = "Something went wrong";

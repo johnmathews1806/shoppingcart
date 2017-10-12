d0b4931942e274f48281a0272338cbc60471ae16
JMATHEWS <JMATHEWS@JMATHEWS-IN.in.oracle.com>
2017-10-12 19:30:21 +0530

    added cart

--- src/main/webapp/products/products.controller.js
+++ src/main/webapp/products/products.controller.js
@@ -3,6 +3,8 @@ angular.module('Products')
 
 	//alert("started controller");
 	
+	$scope.grandTotal = 0;
+	
 	$http.get("http://localhost:3000/products")
 	.then(function (response) {
 		//alert("in products");
@@ -13,19 +15,33 @@ angular.module('Products')
     	//alert($scope.error);    	
 	});
 	
+	$scope.addToCart = function(product_name,price,num){
+		$scope.cart = $scope.cart || [];
+		//alert("called addToCart: "+product_name);
+		$scope.cart.push({"product_name":product_name,"price":price,"quantity":num});
+		$scope.grandTotal = $scope.grandTotal + (price*num);
+		
+	}
+	
+	$scope.Range = function(start, end) {
+	    var result = [];
+	    for (var i = start; i <= end; i++) {
+	        result.push(i);
+	    }
+	    return result;
+	};
+	
 	$scope.order = function(){		
 		//alert("order called");
 		$scope.orderArray = [];
-		angular.forEach($scope.products, function(product){
-			//alert(product.product_name+' - '+product.selected);
-			if(product.selected){	
-				$scope.orderArray.push({"product_name":product.product_name});				
-			}			
+		angular.forEach($scope.cart, function(order){
+			//alert(order.product_name);			
+			$scope.orderArray.push({"product_name":order.product_name,"price":order.price,"quantity":order.quantity});
 		});		
 		//alert($scope.orderArray);		
 		var order_id  = Math.floor(Math.random()*10000);
 		//alert(order_id);		
-		var order_object = {"id":order_id,"order":$scope.orderArray};
+		var order_object = {"id":order_id,"order":$scope.orderArray};		
 		//alert(order_object);
 		var order = JSON.stringify(order_object);		
 		//alert(order);
